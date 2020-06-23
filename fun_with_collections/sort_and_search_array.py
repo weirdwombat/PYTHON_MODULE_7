faa456a350a4e838a4cedec2bef863330f159966
weirdwombat <omkennedy@dmacc.edu>
2020-06-23 14:50:34 -0500

    Pseudocode

---
+++ fun_with_collections/sort_and_search_array.py
@@ -0,0 +1,17 @@
+import array
+
+def search_array():
+    #array.index(x)
+    pass
+
+def key_value(param):
+    #return param[some number???]
+    pass
+
+
+def sort_array():
+    #list_name.sort(key=key_value)
+    #write now my sort_array doesn't have a return value
+    #because I believe that since my array won't be stored in this
+    #function it should update fine. I may change my answer on this.
+    pass
---
+++ test_sort_and_search_array.py
@@ -0,0 +1,19 @@
+import unittest
+#do i need to import array here as well
+import sort_and_search_array
+
+
+class MyTestCase(unittest.TestCase):
+    def test_sort_found(self):
+        #need to be passing a list in
+        self.assertEqual(True, False)
+        #if i have [0, 3, 4, 0]
+        #then my array.index[0] would give me [0]
+
+    def test_sort_not_found(self):
+        #if i have [0, 3, 4, 0]
+        #then my expected output would be negative 1
+
+
+if __name__ == '__main__':
+    unittest.main()
